dfd5617c767d4e65a4b1c7457c9d379fbfa8b418
Andrea Cecchini <cecchiniandrea@outlook.it>
2025-10-27 16:57:12 +0100

    refactor: line comment was too long

--- listings/collektive/localAverage.kt
+++ listings/collektive/localAverage.kt
@@ -1,5 +1,5 @@
 /**
- * Computes the local average of vectors in the neighborhood of the current device.
+ * Computes the local average of vectors in the neighborhood of the current device
  */
 fun Aggregate<Int>.localAverage(v: Point3D): Point3D = with(neighboring(v).all) {
     fold(vectorZero) { acc, nbr -> acc + nbr.value } / size.toDouble()
