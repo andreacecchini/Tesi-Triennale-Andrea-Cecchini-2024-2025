6b4faba7239a825e05e23a17efb5564e4a86e18e
Andrea Cecchini <cecchiniandrea@outlook.it>
2025-10-31 12:45:49 +0100

    abstract and conclusion have been added

--- listings/collektive/GradientAutomaticallyAligned.kt
+++ listings/collektive/GradientAutomaticallyAligned.kt
@@ -1,6 +1,8 @@
-fun <ID: Any> Aggregate<ID>.distanceTo(source: Boolean, metric: Field<ID, Double>) =
-    share(Double.POSITIVE_INFINITY) { distances ->
-        val throughNeighbor = distances.alignedMapValues(metric, Double::plus)
+fun <ID: Any> Aggregate<ID>.distanceTo(
+    source: Boolean,
+    metric: Field<ID, Double>,
+) = share(Double.POSITIVE_INFINITY) { distances ->
+        val throughNeighbor = distances
+            .alignedMapValues(metric, Double::plus)
         if (source) 0.0 else throughNeighbor
     }
-}
--- listings/collektive/GradientManuallyAligned.kt
+++ listings/collektive/GradientManuallyAligned.kt
@@ -1,12 +1,15 @@
-fun <ID: Any> Aggregate<ID>.distanceTo(source: Boolean, metric: Field<ID, Double>) =
-    alignedOn("Aggregate.distanceTo(Boolean)") { // We need to manually align to avoid clashing with other functions with a similar structure
+fun <ID: Any> Aggregate<ID>.distanceTo(
+    source: Boolean,
+    metric: Field<ID, Double>,
+) = alignedOn("Aggregate.distanceTo(Boolean)") { // Avoid clashing with other functions with a similar structure
         share(Double.POSITIVE_INFINITY) { distances ->
-            alignedOn("share(Boolean)") { // We need to manually align again
-                val actualMetrics = project(metric) // The field comes from another context, hence needs projection
-                val throughNeighbor = distances.alignedMapValues(actualMetrics, Double::plus)
+            alignedOn("share(Boolean)") { // We need to manually align again on share operator.
+                val actualMetrics = project(metric) // Fields need projection  
+                val throughNeighbor = distances
+                    .alignedMapValues(actualMetrics, Double::plus)
                 when {
-                    source -> alignedOn(true) { 0.0 }
-                    else -> alignedOn(false) { throughNeighbor } // We cannot run the computation here or the source will never send data!
+                    source -> alignedOn(true) { 0.0 } // Align on true
+                    else -> alignedOn(false) { throughNeighbor } // Align on false
                 }
             }
     }
